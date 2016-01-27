0fd5ad307332474c659bb3d9bf95fdf4c95eabc4
g-yonchev <georgi.d.yonchev@gmail.com>
2016-01-28 00:19:13 +0200

    Added Page MyEvents

---
+++ src/VeloEventsManager.Web/Events/MyEvents.aspx
@@ -0,0 +1,27 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="MyEvents.aspx.cs" Inherits="VeloEventsManager.Web.Events.MyEvents" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+	<asp:GridView
+		runat="server"
+		ID="GridViewMyEvents"
+		ItemType="VeloEventsManager.Models.Event"
+		SelectMethod="GridViewMyEvents_GetData"
+		UpdateMethod="GridViewMyEvents_UpdateItem"
+		AutoGenerateColumns="false"
+		AllowPaging="true"
+		PageSize="5"
+		AllowSorting="true">
+		<Columns>
+			<asp:BoundField HeaderText="Name" DataField="Name" SortExpression="Name"/>
+			<asp:BoundField HeaderText="Description" DataField="Description" />
+			<asp:BoundField HeaderText="Start" DataField="StartDate" SortExpression="StartDate"/>
+			<asp:BoundField HeaderText="End" DataField="EndDate" SortExpression="EndDate"/>
+			<asp:BoundField HeaderText="Distance" DataField="TotalDistance" SortExpression="TotalDistance"/>
+			<asp:BoundField HeaderText="Price" DataField="PriceInLeva" SortExpression="PriceInLeva"/>
+		</Columns>
+		<EmptyDataTemplate>
+			<h3>No events.</h3>
+		</EmptyDataTemplate>
+	</asp:GridView>
+</asp:Content>
---
+++ src/VeloEventsManager.Web/Events/MyEvents.aspx.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using VeloEventsManager.Data;
+using VeloEventsManager.Models;
+
+namespace VeloEventsManager.Web.Events
+{
+	public partial class MyEvents : System.Web.UI.Page
+	{
+		private VeloEventsManagerDbContext dbContext;
+
+		public MyEvents()
+		{
+			this.dbContext = new VeloEventsManagerDbContext();
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		// The return type can be changed to IEnumerable, however to support
+		// paging and sorting, the following parameters must be added:
+		//     int maximumRows
+		//     int startRowIndex
+		//     out int totalRowCount
+		//     string sortByExpression
+		public IQueryable<Event> GridViewMyEvents_GetData()
+		{
+			var userId = this.User.Identity.GetUserId();
+			var user = this.dbContext.Users.FirstOrDefault(x => x.Id == userId);
+
+			if (user == null)
+			{
+				return null;
+			}
+
+			return user.Events.AsQueryable();
+		}
+
+		// The id parameter name should match the DataKeyNames value set on the control
+		public void GridViewMyEvents_UpdateItem(int id)
+		{
+			VeloEventsManager.Models.Event item = null;
+			// Load the item here, e.g. item = MyDataLayer.Find(id);
+			if (item == null)
+			{
+				// The item wasn't found
+				ModelState.AddModelError("", String.Format("Item with id {0} was not found", id));
+				return;
+			}
+			TryUpdateModel(item);
+			if (ModelState.IsValid)
+			{
+				// Save changes here, e.g. MyDataLayer.SaveChanges();
+				this.dbContext.SaveChanges();
+			}
+		}
+	}
+}
