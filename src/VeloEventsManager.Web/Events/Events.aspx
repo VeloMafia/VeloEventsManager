956af72a8c640b42354cc2ce489e2b891be1cfcc
g-yonchev <georgi.d.yonchev@gmail.com>
2016-01-27 23:37:43 +0200

    Added All Events

---
+++ src/VeloEventsManager.Web/Events/Events.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Events.aspx.cs" Inherits="VeloEventsManager.Web.Events.Events" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+	<link type="text/css" href="../Content/ButtonsSort.css" rel="stylesheet" />
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+	<h1>All events</h1>
+	<asp:ListView
+		runat="server"
+		ID="ListViewEvents"
+		ItemType="VeloEventsManager.Models.Event"
+		SelectMethod="ListViewEvents_GetData"
+		OnSorting="ListViewEvents_Sorting">
+		<LayoutTemplate>
+			<div class="row">
+				<asp:LinkButton runat="server" ID="ButtonSortByName" CssClass="col-md-2 btn btn-default" Text="Sort by Name" CommandArgument="Name" CommandName="Sort" />
+				<asp:LinkButton runat="server" ID="ButtonSortByStartDate" CssClass="col-md-2 btn btn-default" Text="Sort by Start Date" CommandArgument="StartDate" CommandName="Sort" />
+				<asp:LinkButton runat="server" ID="ButtonSortByParticipants" CssClass="col-md-2 btn btn-default" Text="Sort by Participants" CommandArgument="Participants.Count" CommandName="Sort" />
+				<asp:LinkButton runat="server" ID="ButtonSortByPrice" CssClass="col-md-2 btn btn-default" Text="Sort by Price" CommandArgument="PriceInLeva" CommandName="Sort" />
+				<asp:LinkButton runat="server" ID="ButtonSortByDistance" CssClass="col-md-2 btn btn-default" Text="Sort by Distance" CommandArgument="TotalDistance" CommandName="Sort" />
+			</div>
+			<asp:PlaceHolder runat="server" ID="itemPlaceHolder"></asp:PlaceHolder>
+			<div class="row">
+				<asp:DataPager runat="server" ID="DataPagerEvents" PagedControlID="ListViewEvents" PageSize="5">
+					<Fields>
+						<asp:NextPreviousPagerField ShowFirstPageButton="true" ShowPreviousPageButton="true" ShowNextPageButton="false" ShowLastPageButton="false" ButtonCssClass="btn btn-primary" />
+						<asp:NumericPagerField />
+						<asp:NextPreviousPagerField ShowFirstPageButton="false" ShowPreviousPageButton="false" ShowNextPageButton="true" ShowLastPageButton="true" ButtonCssClass="btn btn-primary" />
+
+					</Fields>
+				</asp:DataPager>
+			</div>
+		</LayoutTemplate>
+		<ItemTemplate>
+			<h1>Event: <asp:HyperLink runat="server" NavigateUrl='<%# "~/Events/Details?id=" + Item.Id %>'><%#: Item.Name %></asp:HyperLink></h1>
+			<div>Participants: <%#: Item.Participants.Count %></div>
+			<div>Price: <%#: Item.PriceInLeva%></div>
+			<div>TotalDistance: <%#: Item.TotalDistance%></div>
+			<div>StartDate: <%#: Item.StartDate%></div>
+			<div>EndDate: <%#: Item.EndDate%> </div>
+		</ItemTemplate>
+	</asp:ListView>
+</asp:Content>
---
+++ src/VeloEventsManager.Web/Events/Events.aspx.cs
@@ -0,0 +1,174 @@
+﻿namespace VeloEventsManager.Web.Events
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Linq;
+	using System.Linq.Dynamic;
+	using System.Web.ModelBinding;
+	using System.Web.UI.WebControls;
+
+	using VeloEventsManager.Data;
+	using VeloEventsManager.Models;
+
+	public partial class Events : System.Web.UI.Page
+	{
+		private IVeloEventsManagerDbContext dbContext;
+		private bool changeDirection = false;
+
+		public Events()
+		{
+			this.dbContext = new VeloEventsManagerDbContext();
+		}
+
+		public SortDirection SortDirection
+		{
+			get
+			{
+				SortDirection direction = SortDirection.Ascending;
+				if (ViewState["sortdirection"] != null)
+				{
+					if ((SortDirection)ViewState["sortdirection"] == SortDirection.Descending &&
+						!this.changeDirection ||
+						(SortDirection)ViewState["sortdirection"] == SortDirection.Ascending &&
+						this.changeDirection)
+					{
+						direction = SortDirection.Descending;
+					}
+				}
+
+				ViewState["sortdirection"] = direction;
+				return direction;
+			}
+			set
+			{
+				ViewState["sortdirection"] = value;
+			}
+		}
+
+		protected void Page_Load(object sender, EventArgs e)
+		{
+
+		}
+
+		// The return type can be changed to IEnumerable, however to support
+		// paging and sorting, the following parameters must be added:
+		//     int maximumRows
+		//     int startRowIndex
+		//     out int totalRowCount
+		//     string sortByExpression
+		public IQueryable<Event> ListViewEvents_GetData([ViewState("OrderBy")]String OrderBy = null)
+		{
+			var events = this.dbContext.Events.OrderBy(x => x.Id).AsQueryable();
+			if (OrderBy != null)
+			{
+				switch (this.SortDirection)
+				{
+					case SortDirection.Ascending:
+						events = events.OrderBy(OrderBy);
+						break;
+					case SortDirection.Descending:
+						events = events.OrderBy(OrderBy + " Descending");
+						break;
+					default:
+						events = events.OrderBy(OrderBy + " Descending");
+						break;
+				}
+
+				return events;
+			}
+			else
+			{
+				events.OrderBy("StartDate Descending");
+			}
+
+			return events.AsQueryable();
+		}
+
+		protected void ListViewEvents_Sorting(object sender, ListViewSortEventArgs e)
+		{
+			e.Cancel = true;
+			if (ViewState["OrderBy"] != null &&
+				(string)ViewState["OrderBy"] == e.SortExpression)
+			{
+				this.changeDirection = true;
+			}
+			else
+			{
+				this.SortDirection = SortDirection.Ascending;
+			}
+
+			ViewState["OrderBy"] = e.SortExpression;
+			this.ListViewEvents.DataBind();
+
+			this.ChangeButtonSortColor(e.SortExpression);
+		}
+
+		private void ChangeButtonSortColor(string sortExpression)
+		{
+			try
+			{
+				var btns = new List<LinkButton>()
+				{
+					(LinkButton)this.ListViewEvents.Controls[0].Controls[1],
+					(LinkButton)this.ListViewEvents.Controls[0].Controls[3],
+					(LinkButton)this.ListViewEvents.Controls[0].Controls[5],
+					(LinkButton)this.ListViewEvents.Controls[0].Controls[7],
+					(LinkButton)this.ListViewEvents.Controls[0].Controls[9],
+				};
+
+				foreach (var btn in btns)
+				{
+					if (btn.CommandArgument == sortExpression)
+					{
+						var hasBtnSortAscClass = btn.CssClass.Contains("btn-sort-ascending");
+						var hasBtnSortDescClass = btn.CssClass.Contains("btn-sort-descending");
+
+						if (!hasBtnSortAscClass && !hasBtnSortDescClass)
+						{
+							btn.CssClass = btn.CssClass + " btn-sort-ascending";
+						}
+						else if (hasBtnSortAscClass)
+						{
+							// remove asc
+							// add desc
+							int index = btn.CssClass.IndexOf("btn-sort-ascending");
+							btn.CssClass = (index < 0)
+								? btn.CssClass
+								: btn.CssClass.Remove(index, "btn-sort-ascending".Length);
+
+							btn.CssClass = btn.CssClass + " btn-sort-descending";
+						}
+						else if (hasBtnSortDescClass)
+						{
+							// remove desc
+							// add acs
+							int index = btn.CssClass.IndexOf("btn-sort-descending");
+							btn.CssClass = (index < 0)
+								? btn.CssClass
+								: btn.CssClass.Remove(index, "btn-sort-descending".Length);
+
+							btn.CssClass = btn.CssClass + " btn-sort-ascending";
+						}
+					}
+					else
+					{
+						// remove acs
+						// remove desc
+						int indexAsc = btn.CssClass.IndexOf("btn-sort-ascending");
+						btn.CssClass = (indexAsc < 0)
+							? btn.CssClass
+							: btn.CssClass.Remove(indexAsc, "btn-sort-ascending".Length);
+
+						int indexDesc = btn.CssClass.IndexOf("btn-sort-descending");
+						btn.CssClass = (indexDesc < 0)
+							? btn.CssClass
+							: btn.CssClass.Remove(indexDesc, "btn-sort-descending".Length);
+					}
+				}
+			}
+			catch (Exception)
+			{
+			}
+		}
+	}
+}
